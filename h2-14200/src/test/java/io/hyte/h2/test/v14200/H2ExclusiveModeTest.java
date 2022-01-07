8ad14b7cc3d9b0ab11e1ecccbd053d053c045247
Matt Pavlovich <matt@hyte.io>
2022-01-06 21:24:05 -0600

    Update query for H2 2.0.x INFORMATION_SCHEMA changes

--- h2-14200/src/test/java/io/hyte/h2/test/v14200/H2ExclusiveModeTest.java
+++ h2-14200/src/test/java/io/hyte/h2/test/v14200/H2ExclusiveModeTest.java
@@ -1,7 +1,15 @@
 package io.hyte.h2.test.v14200;
 
+import java.sql.SQLException;
+import org.junit.Test;
 import io.hyte.h2.test.BaseH2ExclusiveModeTest;
 
 public class H2ExclusiveModeTest extends BaseH2ExclusiveModeTest {
 
+	
+	@Override
+	@Test(expected = org.h2.jdbc.JdbcSQLNonTransientConnectionException.class)
+	public void testSetExclusiveGetExclusiveFails14200() throws SQLException {
+		super.testSetExclusiveGetExclusiveFails14200();
+	}
 }
--- h2-20206/src/test/java/io/hyte/h2/test/v20206/H2ExclusiveModeTest.java
+++ h2-20206/src/test/java/io/hyte/h2/test/v20206/H2ExclusiveModeTest.java
@@ -1,7 +1,39 @@
 package io.hyte.h2.test.v20206;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import io.hyte.h2.test.BaseH2ExclusiveModeTest;
 
 public class H2ExclusiveModeTest extends BaseH2ExclusiveModeTest {
 
+	@Override
+	protected void setExclusiveMode(Connection connection, int exclusiveMode) throws SQLException {
+		if (!EXCLUSIVE_MODES.contains(exclusiveMode)) {
+			throw new IllegalArgumentException("Unsupported exclusive mode: " + exclusiveMode);
+		}
+		
+		String sql = "SET EXCLUSIVE " + exclusiveMode;
+		
+		try (PreparedStatement statement = connection.prepareStatement(sql)) {
+			statement.execute();
+		}
+	}
+	
+	@Override
+	protected boolean getExclusiveMode(Connection connection) throws SQLException{
+		boolean exclusiveMode = false;
+		
+		String sql = "SELECT SETTING_VALUE FROM INFORMATION_SCHEMA.Settings WHERE SETTING_NAME = 'EXCLUSIVE'";
+		try (PreparedStatement statement = connection.prepareStatement(sql)) {
+			ResultSet result = statement.executeQuery();
+			if (result.next()) {
+				exclusiveMode = result.getBoolean("SETTING_VALUE");
+			}
+		}
+		
+		return exclusiveMode;
+	}
 }
